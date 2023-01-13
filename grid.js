334ccf0721e401e3dd3e8b20a132e0f6d8b75235
aimunNaharr <aimunnaharr@gmail.com>
2023-01-13 11:12:58 +0600

    tile class has created and set the styles

--- grid.js
+++ grid.js
@@ -3,22 +3,53 @@ const CELL_SIZE = 20;
 const CELL_GAP = 2;
 
 export default class Grid {
+	#cells;
 	constructor(gridElement) {
 		gridElement.style.setProperty("--grid-size", `${GRID_SIZE}`);
 		gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
 		gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
-		createCellElement(gridElement)
+		// let's create some cells
+		this.#cells = createCellElement(gridElement).map((cellElement, i) => {
+			return new Cell(cellElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE));
+		});
+	}
+	get #emptyCells() {
+		return this.#cells.filter((cell) => cell.tile == null);
+	}
+	randomEmptyCell() {
+		const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+		return this.#emptyCells[randomIndex];
 	}
 }
 
-function createCellElement(gridElement){
-	const cells=[]
-	for (let i = 0; i <GRID_SIZE* GRID_SIZE ; i++) {
-		const cell=document.createElement('div')
-		cell.classList.add('cell')
-		gridElement.append(cell)
-		cells.push(cell)
-		
+class Cell {
+	#cellElement;
+	#x;
+	#y;
+	#tile;
+	constructor(cellElement, x, y) {
+		this.#cellElement = cellElement;
+		this.#x = x;
+		this.#y = y;
 	}
-	return cells
+	get tile() {
+		return this.#tile;
+	}
+	set tile(value) {
+		this.#tile = value;
+		if (value == null) return;
+		this.tile.x = this.#x;
+		this.tile.y = this.#y;
+	}
+}
+
+function createCellElement(gridElement) {
+	const cells = [];
+	for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+		const cell = document.createElement("div");
+		cell.classList.add("cell");
+		gridElement.append(cell);
+		cells.push(cell);
+	}
+	return cells;
 }
